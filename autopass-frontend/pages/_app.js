ed7d2df30cf39837113436c9a2896194e8fdd214
Kris Haamer <kris@haam.co>
2023-04-22 22:49:59 +0800

    Force dark theme

--- autopass-frontend/pages/_app.js
+++ autopass-frontend/pages/_app.js
@@ -1,25 +1,28 @@
 import { MoralisProvider } from "react-moralis"
 import { NotificationProvider } from "web3uikit"
 import { App } from "konsta/react"
+import { ThemeProvider } from "next-themes"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider initializeOnMount={false}>
-      <NotificationProvider>
-        <div className="grid items-center justify-center h-screen">
-          <div className="mockup-phone border-primary">
-            <div className="camera"></div>
-            <div className="display">
-              <div className="artboard artboard-demo phone-1">
-                <App theme="ios">
-                  <Component {...pageProps} />
-                </App>
+      <ThemeProvider forcedTheme="dark">
+        <NotificationProvider>
+          <div className="grid items-center justify-center h-screen">
+            <div className="mockup-phone border-primary">
+              <div className="camera"></div>
+              <div className="display">
+                <div className="artboard artboard-demo phone-1">
+                  <App theme="ios">
+                    <Component {...pageProps} />
+                  </App>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </NotificationProvider>
+        </NotificationProvider>
+      </ThemeProvider>
     </MoralisProvider>
   )
 }
